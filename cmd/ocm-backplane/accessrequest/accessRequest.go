a4958a7d386ca5d767b896b45990b6c35abfde9f
Brian Smith <bsmit@redhat.com>
2025-05-06 13:28:21 -0400

    Add PersistentPreRun to accessrequest command to handle --govcloud flag

--- cmd/ocm-backplane/accessrequest/accessRequest.go
+++ cmd/ocm-backplane/accessrequest/accessRequest.go
@@ -1,7 +1,10 @@
 package accessrequest
 
 import (
+	"fmt"
+	"os"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 func NewAccessRequestCmd() *cobra.Command {
@@ -10,6 +13,15 @@ func NewAccessRequestCmd() *cobra.Command {
 		Aliases:      []string{"accessRequest", "accessrequest", "accessrequests"},
 		Short:        "Manages access requests for clusters on which access protection is enabled",
 		SilenceUsage: true,
+		PersistentPreRun: func(cmd *cobra.Command, args []string) {
+			// Check if the --govcloud flag is set
+			if viper.GetBool("govcloud") {
+				fmt.Println("Skipping accessrequest command because --govcloud is set")
+				// Skip the rest of the command execution
+				cmd.Help() // Optionally show help or a message
+				os.Exit(0) // Exit the program to skip further execution
+			}
+		},
 	}
 
 	// cluster-id Flag
